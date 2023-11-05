6025c23014b7a61857c86c27f4823c7a7ae1c84b
David LaJambe <dlajambe@gmail.com>
2023-11-05 11:01:29 -0500

    Add version code generation for css files

--- website/__init__.py
+++ website/__init__.py
@@ -10,12 +10,20 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    # Bundle paths are relative to the project's 'static' directory
-    # This is required to compile the project's SASS files into 
-    # CSS files that can be used by a browser
+    """Bundle paths are relative to the project's 'static' directory.
+    This is required to compile the project's SASS files into CSS files
+    that can be used by a browser.
+
+    Updating the version number upon each compilation ensures that users
+    are forced to use the new version when the css file changes instead
+    of the old version that was cachced by their browser the last time
+    they loaded the page.
+    """
     assets = Environment(app)
-    scss = Bundle('scss/main.scss', filters='libsass', output='gen/main.css')
-    
+    scss = Bundle(
+        'scss/main.scss', 
+        filters='libsass', 
+        output='gen/main.%(version)s.css')
     assets.register('main_css', scss)
     return app
 
