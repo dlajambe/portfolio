7fc33ceb3d813e7c26a73d201a23452fb0a791ea
David LaJambe <dlajambe@gmail.com>
2023-11-05 10:47:14 -0500

    Add compilation of css files

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
+from .views import views
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'my key'
 
-    from .views import views
-    from .auth import auth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    # Bundle paths are relative to the project's 'static' directory
+    # This is required to compile the project's SASS files into 
+    # CSS files that can be used by a browser
+    assets = Environment(app)
+    scss = Bundle('scss/main.scss', filters='libsass', output='gen/main.css')
+    
+    assets.register('main_css', scss)
     return app
 
