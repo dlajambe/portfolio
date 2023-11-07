d949f9fa3c82d38eef93164b5f1c4a1b521eb7bd
David LaJambe <dlajambe@gmail.com>
2023-11-07 18:17:39 -0500

    Rename home to index as per convention

--- website/views.py
+++ website/views.py
@@ -3,8 +3,8 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 @views.route('/about')
 def about():
