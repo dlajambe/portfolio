362cc5bb6e33b76f448dd9ec5db54713609f8941
David LaJambe <dlajambe@gmail.com>
2023-12-03 10:02:16 -0500

    Improve card scaling with screen size

--- website/static/js/hamburger.js
+++ website/static/js/hamburger.js
@@ -1,5 +1,3 @@
-//const hamburger = document.querySelector(".hamburger");
-//const navMenu = document.querySelector(".nav-menu");
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".nav-menu");
 hamburger.addEventListener('click', dropMenu);
@@ -8,11 +6,3 @@ function dropMenu() {
     navmenu.classList.toggle("active");
     hamburger.classList.toggle("active");
 }
-
-//hamburger.addEventListener("click", mobileMenu);
-// test.addEventListener("click", testClick);
-
-// function mobileMenu() {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// }
