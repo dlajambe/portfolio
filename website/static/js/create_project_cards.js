2718bd6dfa2714df2037baefb42fd3fbed390ac1
David LaJambe <dlajambe@gmail.com>
2023-12-30 14:43:40 -0500

    Add sentiment analysis project

--- website/static/js/create_project_cards.js
+++ website/static/js/create_project_cards.js
@@ -5,17 +5,26 @@ const projectCards = [
     url: 'https://github.com/dlajambe/llm-from-scratch',
     image_path: '../static/images/llm_from_scratch.jpg',
     title: 'LLM from scratch',
-    body: 'Building a GPT-inspired language model from scratch with generative AI.',
+    body: 'Building a GPT-inspired deep language model from scratch with generative AI.',
     tags: ['NLP', 'PyTorch', 'Python']
   },
 
   {
     id: 'card-portfolio-website', 
     url: 'https://github.com/dlajambe/portfolio',
-    image_path: '../static/images/portfolio.jpg',
+    image_path: '../static/images/portfolio.svg',
     title: 'Portfolio website',
-    body: 'Making and styling my personal Flask-based portfolio website.',
+    body: 'Making and styling my personal portfolio website, powered by Flask.',
     tags: ['HTML', 'CSS', 'JS', 'Python']
+  },
+
+  {
+    id: 'card-sentiment-transformer', 
+    url: '/',
+    image_path: '../static/images/sentiment_transformer.svg',
+    title: 'Sentiment transformer',
+    body: 'Creating a deep transformer model to classify movie reviews as positive or negative.',
+    tags: ['NLP', 'PyTorch', 'Python']
   },
 ];
 
